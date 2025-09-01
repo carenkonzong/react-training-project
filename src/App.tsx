a73636a48f84e6499cf4ec018512f9746d2e4265
Edwin Tsafack Nkonzong <tsafackedwin@gmail.com>
2025-09-01 09:18:04 -0400

    Create portfolio project and few components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
-import { Counter, Counter2, LiveTextDisplay } from "./components/UseState";
+import Hero_section from "./Portfolio components/Hero_section";
+import My_projects from "./Portfolio components/My_projects";
+import Navbar from "./Portfolio components/Navbar";
+import Skill_section from "./Portfolio components/Skill_section";
 
 function App() {
   return (
     <>
-      <Counter />
-      <Counter2 />
-      <LiveTextDisplay />
+      <Navbar />
+      <Hero_section />
+      <Skill_section />
+      <My_projects />
     </>
   );
 }
 
 export default App;
+
+{
+  /* import {
+  Counter,
+  Counter2,
+  LiveTextDisplay,
+  ToggleVisibility,
+  ColorChanger,
+} from "./components/UseState";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home"; */
+}
+
+/* 
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/counter2" element={<Counter2 />} />
+        <Route path="/LiveTextDisplay" element={<LiveTextDisplay />} />
+        <Route path="/ToggleVisibility" element={<ToggleVisibility />} />
+        <Route path="/ColorChanger" element={<ColorChanger />} />
+      </Routes>
+      */
---
+++ src/Portfolio components/Hero_section.tsx
@@ -0,0 +1,41 @@
+function Hero_section() {
+  return (
+    <>
+      <div className="flex w-full justify-center">
+        <div className="w-full max-w-[1700px] items-center grid grid-cols-2 gap-20">
+          <div className="w-9/10 px-15">
+            <div className="text-5xl mb-5">Hi, I'm Edwin Nkonzong</div>
+            <div className="text-6xl font-extrabold mb-7  bg-gradient-to-r bg-clip-text text-transparent from-[#70e3d2] to-[#0aebc9]">
+              Front-End Developer
+            </div>
+            <div className="text-2xl mb-10 text font-sans">
+              I build responsive and scalable web applications using React and
+              TypeScript. I specialize in writing clean, maintainable code,
+              optimizing performance, and delivering seamless user experiences.
+            </div>
+            <button className="p-3 text-white font-bold text-lg bg-gradient-to-b from-[#97e1f7] to-[#3ccdf9] rounded-2xl">
+              See Projects
+            </button>
+          </div>
+          <div className="flex justify-start">
+            <div className="flex justify-center flex-col">
+              <img src="/image.png" alt="image" className="w-150" />
+              <div
+                className="flex flex-row justify-around items-center self-center rounded-2xl px-6 py-3 
+              bg-gradient-to-r from-white/25 via-white/15 to-white/10
+              backdrop-blur-xl border border-white/30 shadow-lg shadow-black/10 
+              [background-clip:padding-box]"
+              >
+                <div className="w-30">10+ years industry Experience</div>
+                <div className="w-30">50+ Code industry projects</div>
+                <div className="w-30">100+ star industry review</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Hero_section;
